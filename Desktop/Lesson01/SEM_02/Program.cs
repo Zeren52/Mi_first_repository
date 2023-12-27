0167956b940bcfe4f5bbbbbd67582b5235d213a0
Dmitry Serov <serovdmitri1997@mail.ru>
2023-12-27 10:51:44 +0300

    git add SEM_02

---
+++ Desktop/Lesson01/SEM_02/Program.cs
@@ -0,0 +1,20 @@
+﻿int i1 = 7;
+int i2 = 9;
+int sum = i1 + i2;
+Console.WriteLine(sum); 
+
+
+long l1 = 4;
+long sum1 = i1 + l1;
+Console.WriteLine(sum1);
+
+double d1 = 7.7;
+double sum2 = d1 + i1;
+Console.WriteLine(sum2);
+
+int product1 = i1 * i2;
+long product2 = l1 * i1;
+double product3 = i1 * d1;
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
