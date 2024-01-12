2f15ae43c1b13ebe2e2c3ef065fc7dc7ef3c57d4
Dmitry Serov <serovdmitri1997@mail.ru>
2024-01-12 21:56:36 +0300

    add len_3

---
+++ Desktop/Новые_проекты/семинар_3/len_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найдите произведения пар чисел в одномерном массиве. Парой
+// считаем первый и последний элемент, второй и предпоследний и
+// т.д. Результат запишите в новый массив.
+// Пример
+// [1 3 2 4 2 3] => [3 6 8]
+// [2 3 1 7 5 6 3] => [6 18 5] (элемент 7 не имеет пары)
+using System.ComponentModel.DataAnnotations;
+
+int[] array = { 11, 22, 33, 44, 55, 66, 77 };
+int[] result = new int[array.Length / 2]; // 7 / 2 = 3
+// Массив result, состоящий из 3 элементов: [0,0,0]
+for (int i = 0, j = array.Length - 1; i < result.Length; i++, j-- )
+{
+    result[i] = array[i] * array[j];
+}
+Console.WriteLine($"Результат: [{string.Join(";", result)}]");
