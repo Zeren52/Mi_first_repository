b04ec979797bb87b86f2c95282c4df80f979d72c
Dmitry Serov <serovdmitri1997@mail.ru>
2024-01-12 15:04:06 +0300

    add task_3

---
+++ Desktop/Новые_проекты/task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] array = { 2, 5, 34, 565, 35, 76, 23, 14, 24, 245 };
+int i = 0;
+
+while (i < n)
+{
+    if(array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
