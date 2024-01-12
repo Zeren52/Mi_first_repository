ea926d628a794725ea13469bec2f5e4ea7de87c3
Dmitry Serov <serovdmitri1997@mail.ru>
2024-01-12 14:45:04 +0300

    add task2

---
+++ Desktop/Новые_проекты/task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] Array = new int[n];
+int i = 0;
+
+while(i < n)
+{
+    Array[i] = i + 1;
+    // Console.Write(Array [i]);
+    // Console.Write(' ');
+    Console.Write($"{Array[i]} ");
+    i = i + 1;
+}
