2abbb45b8c5945d7d991846ad8dac2f1c3372205
Dmitry Serov <serovdmitri1997@mail.ru>
2024-01-12 15:41:17 +0300

    add task_4

---
+++ Desktop/Новые_проекты/task_4/Program.cs
@@ -0,0 +1,34 @@
+﻿// Нахождение максимального элемента в массиве.
+int n = 10;
+int[] array = { 4, 5, 76, 34, 76, 25, 35, 95, 58, 29 };
+int i = 0;
+int max = array[0];
+while(i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
+
+max = array[0];
+for(int j = 0; j < n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+Console.WriteLine(max);
+
+max = array[0];
+foreach(int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+Console.WriteLine(max);
