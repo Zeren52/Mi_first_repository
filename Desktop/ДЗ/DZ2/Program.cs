b6d17051d0ab006b0803b37932f76e30a982a925
Dmitry Serov <serovdmitri1997@mail.ru>
2024-01-17 13:54:24 +0300

    add dz2

--- Desktop/ДЗ/DZ2/Program.cs
+++ Desktop/ДЗ/DZ2/Program.cs
@@ -68,36 +68,93 @@
 
 // int[] array = {0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01}; // Создание массива
 // Выводится: 5.15
+// using System;
+// using System.Linq;
+
+// //Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+// class UserInputToCompileForTest
+// { 
+//     // Подсчет количества четных элементов массива
+//     // numbers - массив, в котором ведется подсчет
+//     public static int CountEvenItems(int[] numbers)
+//     {
+      
+//         int count = 0;
+//       foreach (int number in numbers)
+//         if(number % 2 == 0)
+//          count++;
+//         return count;
+//     }
+    
+//     public static void PrintResult(int[] array)
+//     {
+//         Console.WriteLine(CountEvenItems(array));
+//     }
+// }
+
+// //Не удаляйте и не меняйте класс Answer!
+// class Answer
+// {
+//     public static void Main(string[] args)
+//     {
+//         int[] array;
+        
+
+//         if (args.Length >= 1) {
+//             // Объединяем все аргументы командной строки в одну строку
+//             string joinedArgs = string.Join(" ", args);
+
+//             // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
+//             array = joinedArgs.Split(", ")
+//                                   .Select(int.Parse)
+//                                   .ToArray();
+            
+//             // Теперь arr содержит преобразованные в целые числа из командной строки
+        
+//         } else {
+//            // Если аргументов на входе нет
+//             array = new int[] {2, 5, 2, 3, 6, 3, 7, 8, 2, 4}; // Создание массива
+//         }
+//         UserInputToCompileForTest.PrintResult(array);
+//     }
+// }
+
+
 using System;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 //Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
 class UserInputToCompileForTest
 { 
+    // Разница между максимальным и минимальным элементами массива
+    
+    // Нахождение минимума массива
     public static double FindMin(double[] numbers)
     {
-        int i = 0;
-        double min = numbers[0];
-        foreach (int number in numbers)
-        {
-            if (numbers[i] < min)
-            i++;
-        }
-       return min;
+      
+       double min = numbers[0];
+        double count = 0;
+      foreach (double number in numbers)
+        if(number < min) 
+          min = number;
+         count++;
+        return min;
     }
     
     // Нахождение максимума массива
     public static double FindMax(double[] numbers)
     {
-        double max = numbers[0];
-        foreach (int number in numbers)
-        {
-            if (numbers[i] < max)
-            i++;
-        }
+      double count = 1;
+       double max = numbers[0];
+      foreach (double number in numbers)
+        if(number > max)
+          max = number;
+         count++;
         return max;
     }
+    
+    
+    
     public static void PrintResult(double[] array)
     {
         Console.WriteLine(FindMax(array) - FindMin(array));
@@ -130,3 +187,6 @@ class Answer
         UserInputToCompileForTest.PrintResult(array);
     }
 }
+
+
+
--- Desktop/ДЗ/DZ2/obj/Debug/net8.0/DZ2.AssemblyInfo.cs
+++ Desktop/ДЗ/DZ2/obj/Debug/net8.0/DZ2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DZ2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+251a684ef6d8700b9b98d566059e7f1d2f013c6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7ab5462665e3ef7cd12536035db68334218d522")]
 [assembly: System.Reflection.AssemblyProductAttribute("DZ2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DZ2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
